a4ca8e5c9313d6bcbc2be67f3e9b0ef4ce6187da
themarkness <williamsmarka@gmail.com>
2017-03-13 19:10:54 +0000

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -271,7 +271,7 @@ var $indicators = $('<ol>')
 
   var $social = $("<div>")
     .attr('class', 'results-social')
-    .html('<div id = "social-text">You can now move on to your next location.Share your results with your friends!</div>')
+    .html('<div id = "social-text">You can now move on to your next location!</div>')
     .appendTo($results_slide);
 
   var $twitter_link = $('<a>')
